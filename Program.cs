216373a6cb8c7edb4a709b76bcaebad484ccd701
etrex <et284vu065k3@gmail.com>
2014-11-12 11:07:43 +0800

    RandomList
    a custom class : RandomList for Enumerable test

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+
+namespace EnumerableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enumerable Test :\n");
+            enumableTest();
+            Console.ReadLine();
+        }
+
+        static void enumableTest()
+        {
+            RandomList t = new RandomList();
+            Console.WriteLine("TestList foreach ver:");
+            foreach ( double i in t ) {
+                Console.WriteLine(i);
+            }
+            t.Reset();
+            Console.WriteLine("");
+
+
+            Console.WriteLine("TestList GetEnumerator ver:");
+            var tor = t.GetEnumerator();
+            while ( tor.MoveNext() ) {
+                double i  = tor.Current;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+
+            List<int> list = new List<int>(){1,2,4,3,6,5};
+            IEnumerable<int> ie = list;
+            
+            Console.WriteLine("IEnumerable foreach ver:");            
+            foreach ( int i in ie ) {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+
+            Console.WriteLine("IEnumerable GetEnumerator ver:");
+            var e = ie.GetEnumerator();
+            while ( e.MoveNext() ) {
+                int i  = e.Current;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+
+            Console.WriteLine("List foreach ver:");
+            foreach ( int i in list ) {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("");
+
+
+            Console.WriteLine("List for ver:");
+            for ( int i = 0 ; i < list.Count ; i++ ) {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ RandomList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnumerableTest
+{
+    class RandomList : IEnumerable<double>, IEnumerator<double> 
+    {
+        Random rand = new Random();
+        double current = 1;
+        double min = 0.2;
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return this;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Current
+        {
+            get {
+                current = rand.NextDouble();
+                return current;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool MoveNext()
+        {
+            return current > min;
+        }
+
+        public void Reset()
+        {
+            current = 1;
+        }
+    }
+}
